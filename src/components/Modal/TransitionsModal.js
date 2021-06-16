8d737c01df4d04bb35aaa2c15e165620753c8e8c
Karol Ciołczyk <dyweko@gmail.com>
2021-06-16 09:25:55 +0200

    Modal-component added to components directory, LoginForm component added to components directory, LoginForm added to LoginForm.js from material-ui, TransitionsModal.js added from material-ui

---
+++ src/components/Modal/TransitionsModal.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import { PinDropSharp } from "@material-ui/icons";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  root: {
+    padding: "0 50px",
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(1),
+      // width: theme.spacing(70),
+      height: theme.spacing(80),
+    },
+  },
+}));
+
+export default function TransitionsModal(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleOpen}>
+        react-transition-group
+      </button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Paper className={classes.root} elevation={3}>
+          {props.children}
+          </Paper>
+        </Fade>
+      </Modal>
+    </div>
+  );
+};
