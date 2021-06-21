5d20b2faa9619778d7fd5da8480ee092209525fd
Michal Lachowski <mplachowski@gmail.com>
2021-06-21 20:16:04 +0200

    user-wallpaper element created

---
+++ src/user-profile/user-wallpaper.jsx
@@ -0,0 +1,11 @@
+export function UserWallpaper(props) {
+    
+  return (
+      <img 
+        src={props}
+        alt="user wallpaper"
+        className="profile-userWallpaper"
+      >    
+      </img>  
+  )  
+};
