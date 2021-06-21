9b44b95257138f7ae78320a5b5227423b6ce1572
Michal Lachowski <mplachowski@gmail.com>
2021-06-21 20:22:46 +0200

    started works on profile.jsx

---
+++ src/user-profile/profile.jsx
@@ -0,0 +1,11 @@
+import { UserWallpaper } from "./user-wallpaper";
+
+function handleUserWallpaper() {
+//if user has upload wallpeper use it or get default wallpaper
+};
+
+return (
+    <>
+    <UserWallpaper props={handleUserWallpaper}/>
+    </>
+);
