ef52ccc17f7e37bd7e709048508a0570297dc542
Michal Lachowski <mplachowski@gmail.com>
2021-06-21 20:52:13 +0200

    added avatar component to profile

--- src/user-profile/profile.jsx
+++ src/user-profile/profile.jsx
@@ -1,11 +1,16 @@
+import { UserAvatar } from "./avatar";
 import { UserWallpaper } from "./user-wallpaper";
 
 function handleUserWallpaper() {
 //if user has uploaded wallpaper use it or get default wallpaper
 };
 
+function handleUserAvatar() {
+//if user has uploaded avatar picture use it or get default avatar
+};
 return (
     <>
-    <UserWallpaper props={handleUserWallpaper}/>
+    <UserWallpaper props={handleUserWallpaper} />
+    <UserAvatar props={handleUserAvatar} />
     </>
 );
