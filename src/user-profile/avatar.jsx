97d765d6a9f0fd33db005ac7b2ff2e88bfe10dbf
Michal Lachowski <mplachowski@gmail.com>
2021-06-21 20:46:01 +0200

    created avatar.jsx & works on profile.jsx

---
+++ src/user-profile/avatar.jsx
@@ -0,0 +1,10 @@
+function UserAvatar(props) {
+    return (
+        <img 
+          src={props}
+          alt="user avatar"
+          className="profile-userAvatar"
+        >    
+        </img>  
+    )  
+}
--- src/user-profile/profile.jsx
+++ src/user-profile/profile.jsx
@@ -1,7 +1,7 @@
 import { UserWallpaper } from "./user-wallpaper";
 
 function handleUserWallpaper() {
-//if user has upload wallpeper use it or get default wallpaper
+//if user has uploaded wallpaper use it or get default wallpaper
 };
 
 return (
