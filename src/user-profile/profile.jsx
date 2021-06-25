7a69d796d011adc86a84e82ed093022b7cdd629a
Michal Lachowski <mplachowski@gmail.com>
2021-06-25 15:17:45 +0200

    added styles&tab bar

--- src/user-profile/profile.jsx
+++ src/user-profile/profile.jsx
@@ -1,22 +1,32 @@
 import { UserAvatar } from "./avatar";
 import { UserWallpaper } from "./user-wallpaper";
+import './profile.css';
+import SimpleTabs from "./profileTab";
 
 export function Profile (){
 
 function handleUserWallpaper() {
-    //if user has uploaded wallpaper use it or get default wallpaper
+//if user has uploaded wallpaper use it or get default wallpaper
     return "https://picsum.photos/950/300"
 };
 
 function handleUserAvatar() {
 //if user has uploaded avatar picture use it or get default avatar
-    return "https://picsum.photos/100/100"
+    return "https://picsum.photos/150/150"
 };
 
+function renderUserName(){
+// render user name along avatar pic, if there is no name use placeholder
+    return "Anonymus"    
+}
 return (
     <>
-    <UserWallpaper url={handleUserWallpaper()}/>
-    <UserAvatar url={handleUserAvatar()} />
+    <UserWallpaper url={handleUserWallpaper()} />
+    <div className="userProfile_navBar">
+        <UserAvatar url={handleUserAvatar()} />
+        <span>{renderUserName()}</span>
+    </div>
+    <SimpleTabs />
     </>
 );
 }
