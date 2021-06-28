f10b13aeae7bcd9793ae1fa05bfb1bc03bdf649f
Ilona Kozak <ilonaikozak@gmail.com>
2021-06-28 16:57:07 +0100

    styling the form

--- src/components/Route/CreateRouteForm.js
+++ src/components/Route/CreateRouteForm.js
@@ -6,6 +6,7 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -13,21 +14,28 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     root: {
         '& .MuiTextField-root': {
-            margin: theme.spacing(2),
-            width: '50ch',
+            margin: theme.spacing(1),
         },
+    },
+    container: {
+        marginRight: '0px',
+    },
+    paper: {
+        marginTop: '20px',
+        paddingBottom: '20px',
     },
     title: {
         textTransform: 'uppercase',
         fontWeight: 'bold',
-        marginTop: '40px',
+        paddingTop: '20px',
+        paddingBottom: '10px',
         [theme.breakpoints.up('sm')]: {
             display: 'block',
         },
     },
     formControl: {
         margin: theme.spacing(2),
-        minWidth: '50ch',
+        width: '55ch',
     },
     selectEmpty: {
         marginTop: theme.spacing(2),
@@ -43,69 +51,67 @@ export default function CreateRouteForm() {
     };
 
     return (
-        <Container component="main" maxWidth="md">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Typography
-                    color="primary"
-                    className={classes.title}
-                    variant="h5"
-                    noWrap>
-                    Create New Route
-                </Typography>
+        <Container maxWidth="sm" className={classes.container}>
+            <Paper className={classes.paper}>
+                <CssBaseline />
+                <div>
+                    <Typography
+                        color="primary"
+                        className={classes.title}
+                        variant="h5"
+                        noWrap>
+                        Create New Route
+                    </Typography>
 
-                <form form className={classes.root} noValidate autoComplete="off" >
-                    <div>
-                        <TextField
-                            id="startRoute"
-                            label="Start Route"
-                            variant="outlined"
-                        />
-                        <TextField
-                            id="endRoute"
-                            label="End Route"
-                            variant="outlined"
-                        />
-                        <TextField
-                            id="distance"
-                            label="Distance in km"
-                            variant="outlined"
-                        />
-                        <TextField
-                            id="time"
-                            label="Time in hours"
-                            variant="outlined"
-                        />
-                        <TextField
-                            id="description"
-                            label="Description"
-                            multiline
-                            rows={3}
-                            variant="outlined"
-                        />
-                        <FormControl variant="outlined" className={classes.formControl}>
-                            <InputLabel id="select-region">Region</InputLabel>
-                            <Select
-                                labelId="select-region"
-                                id="select-region"
-                                value={region}
-                                onChange={handleChange}
-                                label="Region"
-                            >
-                                <MenuItem value="">
-                                    <em>None</em>
-                                </MenuItem>
-                                <MenuItem value={1}>Greater Poland</MenuItem>
-                                <MenuItem value={2}>Kuyavia</MenuItem>
-                                <MenuItem value={3}>Mazury</MenuItem>
-                                <MenuItem value={4}>Podhale</MenuItem>
-                                <MenuItem value={5}>Pomerania</MenuItem>
-                                <MenuItem value={6}>Silesia</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </div>
-                </form>
-            </div>
+                    <form form className={classes.root} noValidate autoComplete="off" >
+                        <div>
+                            <TextField className={classes.formControl}
+                                id="routeTitle"
+                                label="Enter a title for your route"
+                                variant="outlined"
+                            />
+                            <TextField className={classes.formControl}
+                                id="description"
+                                label="Description"
+                                multiline
+                                rows={3}
+                                variant="outlined"
+                            />
+                            <TextField
+                                id="distance"
+                                label="Distance in km"
+                                variant="outlined"
+                            />
+                            <TextField
+                                id="time"
+                                label="Time in hours"
+                                variant="outlined"
+                            />
+
+                            <FormControl variant="outlined" className={classes.formControl}>
+                                <InputLabel id="select-region">Region</InputLabel>
+                                <Select
+                                    labelId="select-region"
+                                    id="select-region"
+                                    value={region}
+                                    onChange={handleChange}
+                                    label="Region"
+                                >
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
+                                    <MenuItem value={1}>Greater Poland</MenuItem>
+                                    <MenuItem value={2}>Kuyavia</MenuItem>
+                                    <MenuItem value={3}>Mazury</MenuItem>
+                                    <MenuItem value={4}>Podhale</MenuItem>
+                                    <MenuItem value={5}>Pomerania</MenuItem>
+                                    <MenuItem value={6}>Silesia</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </div>
+                    </form>
+                </div>
+            </Paper>
         </Container>
     );
 }
