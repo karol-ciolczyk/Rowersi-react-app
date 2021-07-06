31cd2a2e8c364f72355c8b1f85d88e43adfd1e23
Michal Lachowski <mplachowski@gmail.com>
2021-07-06 21:19:19 +0200

    added avatar button

--- src/user-profile/ProfileSettings.jsx
+++ src/user-profile/ProfileSettings.jsx
@@ -1,7 +1,17 @@
+import Button from "@material-ui/core/Button";
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+
 export function ProfileSettings() {
     return (
         <>
         <span>HERE BE options to change profile </span>
+        <Button 
+         variant="contained"
+         color="primary"
+         startIcon={<CloudUploadIcon />}
+        >
+            Upload avatar picture
+        </Button>    
         </>
     )
 }
