4a5bc3ae2e1aaed276da6e7fa777a66f75654320
Karol Ciołczyk <dyweko@gmail.com>
2021-07-13 10:35:00 +0200

    .then() added to addRouteDataToFirebase.js

--- src/firebase/addRouteDataToFirebase.js
+++ src/firebase/addRouteDataToFirebase.js
@@ -1,7 +1,12 @@
 import firebase from "firebase";
 
 const addRouteDataToFirebase = (routeDataObject)=>{
-  firebase.firestore().collection("routes").add(routeDataObject).then(console.log);
+  firebase.firestore().collection("routes").add(routeDataObject)
+  .then((response)=>{
+    if(response){
+      alert("your new route added")
+    }
+  });
 }
 
 export default addRouteDataToFirebase;
