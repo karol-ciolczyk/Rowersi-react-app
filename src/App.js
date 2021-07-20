7b211479477af9501707533f4137448715c0ce13
Karol Ciołczyk <dyweko@gmail.com>
2021-07-20 13:04:24 +0200

    initial state of RouteData component finished, router functions added to App.js

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ import UserSessionContext from "./components/context/userSession-context";
 import Navbar from "./components/Navbar";
 import CreateNewRoute from "./components/CreateNewRoute/CreateNewRoute";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams,
+} from "react-router-dom";
 import { Profile } from "./user-profile/profile.jsx";
 import firebase from "firebase";
 import DisplayRouteElements from "./components/DisplayRouteElements/DisplayRouteElements";
+import RouteData from "./components/RouteData/RouteData";
 
 function App() {
   const [userUid, setUserUid] = useState();
+  const [routeId, setRouteId] = useState('')
 
   const onLoggedInDataHandler = (uid) => {
     console.log(uid);
     setUserUid(uid);
   };
 
-  useEffect(() => {  // this is needed to setUserUid after refreshing browser
+  useEffect(() => {
+    // this is needed to setUserUid after refreshing browser
     firebase.auth().onAuthStateChanged(function (user) {
       if (user) {
         console.log(user);
@@ -41,6 +49,9 @@ function App() {
             <Route exact path="/newRoute" component={CreateNewRoute} />
             <Route path="/profile" component={Profile} />
             <Route path="/" component={DisplayRouteElements} />
+            <Route path={`route/${routeId}`}>
+              <RouteData routeId={routeId} />
+            </Route>
             {/* Tu trzeba wstawić inne widoki, czyli tworzenie tras, homepage itp */}
           </Switch>
         </div>
--- src/components/RouteData/RouteData.js
+++ src/components/RouteData/RouteData.js
@@ -1,10 +1,14 @@
 import React, { useRef, useEffect, useState } from "react";
+import firebase from "firebase";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import Directions from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
+import { Avatar, Paper, Typography, Box } from "@material-ui/core";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import Rating from "@material-ui/lab/Rating";
 
-import classes from "./RouteData.module.css"
+import classes from "./RouteData.module.css";
 import style from "./directions-styles";
-
+import { CompassCalibrationOutlined } from "@material-ui/icons";
 
 const RouteData = (props) => {
   const mapContainer = useRef(null);
@@ -12,6 +16,7 @@ const RouteData = (props) => {
   const [lng, setLng] = useState(19.52);
   const [lat, setLat] = useState(50.1);
   const [zoom, setZoom] = useState(11);
+  const [routeData, setRouteData] = useState({});
   // const [routeData, setRouteData] = useState({ distance: "", duration: "" });
 
   const directions = new Directions({
@@ -25,7 +30,7 @@ const RouteData = (props) => {
     congestion: true,
     steps: true,
     controls: {
-      inputs: props.isInput,
+      inputs: false,
       instructions: false,
       profileSwitcher: true,
     },
@@ -82,11 +87,90 @@ const RouteData = (props) => {
     // });
   });
 
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("routes")
+      .doc("X1bY5KGa8oxBwrc1JOVW")
+      .get()
+      .then((response) => {
+        const routeData = response.data();
+        setRouteData(routeData);
+        directions.setOrigin(routeData.origin);
+        directions.setDestination(routeData.destination);
+      });
+  }, []);
+
+  console.log(routeData);
+
   return (
-    <div>
-      <div></div>
-      <div>
-        <div ref={mapContainer} className={classes["map-container"]} />
+    <div className={classes.flexContainer}>
+      <div className={classes.flexchild1}>
+        <header className={classes.header}>
+          <div>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://picsum.photos/150/150"
+              style={{ height: "100px", width: "100px" }}
+            />
+            <div>
+              <h2>User Name</h2>
+            </div>
+          </div>
+          <div className={classes.routeData}>
+            <div>
+              <h1>Route Title: Kraków - Młoszowa-Trzebnia-Chrzanów</h1>
+            </div>
+            <div className={classes.routeNumberDataContainer}>
+              <Paper>
+                <Typography variant="h6" style={{ padding: "20px" }}>
+                  Distance: 100km
+                </Typography>
+              </Paper>
+              <Paper>
+                <Typography variant="h6" style={{ padding: "20px" }}>
+                  Time: 12:45:44
+                </Typography>
+              </Paper>
+              <Paper>
+                <Typography variant="h6" style={{ padding: "20px" }}>
+                  Heighest Point: 350m npm
+                </Typography>
+              </Paper>
+            </div>
+          </div>
+        </header>
+        <div className={classes.routeDescriptionContainer}>
+          <Paper>
+            <Typography variant="body1" style={{ padding: "20px" }}>
+              Route description: Aboute route
+              Trasa fajna pełan wrażeń
+              Na maksa odlotowa polecam każdemu kto chce pojeździć ostro na rowerze
+            </Typography>
+          </Paper>
+        </div>
+        <div className={classes.rateRouteContainer}>
+          <div>
+            <Typography variant="h3" style={{ padding: "20px" }}>
+              4.6
+            </Typography>
+          </div>
+          <div>
+            <Box component="fieldset" mb={3} borderColor="transparent">
+              <Typography component="legend">Custom empty icon</Typography>
+              <Rating
+                name="customized-empty"
+                defaultValue={2}
+                precision={0.5}
+                emptyIcon={<StarBorderIcon fontSize="inherit" />}
+                size="large"
+              />
+            </Box>
+          </div>
+        </div>
+      </div>
+      <div className={classes.flexchild2}>
+        <div ref={mapContainer} className={classes.mapContainer} />
       </div>
     </div>
   );
