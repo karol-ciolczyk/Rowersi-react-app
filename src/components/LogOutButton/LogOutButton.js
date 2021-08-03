8d7c22bed90c93880e5ce623bad9e3f6868de79b
Adam <aszum@poczta.fm>
2021-08-03 14:29:39 +0200

    log out button added to navbar.js

---
+++ src/components/LogOutButton/LogOutButton.js
@@ -0,0 +1,34 @@
+import { Button } from "@material-ui/core";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: "0px 5px",
+    backgroundColor: "#3BB2D0",
+    color: "#fff",
+    "&:hover, &:focus": {
+      backgroundColor: "#3BB2D0",
+    },
+  },
+}));
+
+export const LogOutButton = function () {
+  const classes = useStyles();
+  const onClickHandler = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        alert("You are log out");
+      })
+      .catch((error) => {
+        alert("Error");
+      });
+  };
+  return (
+    <Button className={classes.button} onClick={onClickHandler}>
+      Log Out
+    </Button>
+  );
+};
