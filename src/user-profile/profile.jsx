559992a1ed3c68a31e865c4b00dd9d29d7b91c9f
Michal Lachowski <mplachowski@gmail.com>
2021-08-05 20:33:45 +0200

    rework after reported problem

--- src/user-profile/profile.jsx
+++ src/user-profile/profile.jsx
@@ -6,78 +6,64 @@ import { Switch, Route } from "react-router-dom";
 import { FriendsList } from "./FriendsList";
 import { Dashboard } from "./Dashboard";
 import { ProfileSettings } from "./ProfileSettings";
-import UserSessionContext from '../components/context/userSession-context.js';
+import UserSessionContext from "../components/context/userSession-context.js";
 import { useContext, useState, useEffect } from "react";
 import firebase from "firebase";
 
 export function Profile() {
-
   const userSessionContext = useContext(UserSessionContext);
   const { userUid } = userSessionContext;
- 
-  const [ defaultAvatar, setDefaultAvatar] = useState("https://picsum.photos/300/300")
-  const [ wallpaperUrl, setWallpaperUrl ] = useState("https://picsum.photos/1200/600");
-  const [ avatarUrl, setAvatarUrl ] = useState(defaultAvatar);
-  const [ userName, setUserName ] = useState("Anonymus");
 
-  // handleUserWallpaper(userUid);
-  // handleUserAvatar(userUid);
-  // handleUserName(userUid)
-  // getDefaultAvatar();
-  
+  const [wallpaperUrl, setWallpaperUrl] = useState(
+    "https://picsum.photos/1200/600"
+  );
+  const [avatarUrl, setAvatarUrl] = useState("/assets/defaultAvatar.JPG");
+  const [userName, setUserName] = useState("Anonymus");
+
   useEffect(() => {
-  // function getDefaultAvatar() {
     firebase
-      .storage()
-      .ref("usersTest/defaultAvatar.JPG")
-      .getDownloadURL()
-      .then( url => {
-        setDefaultAvatar(url)})
-      // .catch( error => console.log(error));
-      // };
-    })
-
-  useEffect(() => {
-  // function handleUserWallpaper(uid) {
-      firebase
       .storage()
       .ref("usersTest/" + userUid + "/wallpaper/background.jpg")
       .getDownloadURL()
-      .then( url => {
-        setWallpaperUrl(url)})
-      // .catch( error => console.log(error));
-  // }
+      .then((url) => {
+        setWallpaperUrl(url);
+      });
   }, [userUid]);
 
   useEffect(() => {
-  // function handleUserAvatar(uid) {
     firebase
       .storage()
       .ref("usersTest/" + userUid + "/avatar/avatar.jpg")
       .getDownloadURL()
-      .then( url => {
-        setAvatarUrl(url)})
-      // .catch(error => console.log(error));
-  // }
-}, [userUid]);
+      .then((url) => {
+        setAvatarUrl(url);
+      });
+  }, [userUid]);
 
-useEffect(() => {
-  // function handleUserName(uid) {
-    return firebase
-      .firestore()
-      .collection("usersTest")
-      .doc(userUid)
-      .get()
-      .then( doc => setUserName(doc.data().name))
-      // .catch( error => console.log(error));
-  // }
-}, [userUid]);
+  useEffect(() => {
+    if (userUid) {
+      try {
+        firebase
+          .firestore()
+          .collection("usersTest")
+          .doc(userUid)
+          .get()
+          .then((doc) => {
+            if (doc.data().name) {
+              setUserName(doc.data().name);
+            }
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [userUid]);
 
   return (
     <>
       <UserWallpaper url={wallpaperUrl} />
       <div className="userProfile_navBar">
-        <UserAvatar url={avatarUrl} name={userName}/>
+        <UserAvatar url={avatarUrl} name={userName} />
       </div>
       <UserProfileTabs />
       <div className="userProfile_SwitchArea">
