8f1e5bc992e2a9d4be2a1ba88b4e6cd17beee5c8
Karol Ciołczyk <dyweko@gmail.com>
2021-08-21 11:00:36 +0200

    some style added in UserProfile directory

--- src/components/UserProfile/UserCard.js
+++ src/components/UserProfile/UserCard.js
@@ -4,7 +4,7 @@ import style from "./UserCard.module.css";
 
 export const UserCard = function () {
   return (
-    <Paper elevation={15} className={style.container}>
+    <Paper elevation={12} className={style.container}>
       <div className={style.container__photoContainer}> Photo </div>
       <div className={style.container__userDescription}>User Description</div>
     </Paper>
