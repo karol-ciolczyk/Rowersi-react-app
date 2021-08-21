483e773979936f13b77544ba6a4aea0a807f7ec6
Karol Ciołczyk <dyweko@gmail.com>
2021-08-21 15:55:19 +0200

    more style added in DropMenu.js and DropMenu.module.css

--- src/components/NavbarButtons/DropMenu.js
+++ src/components/NavbarButtons/DropMenu.js
@@ -1,21 +1,34 @@
-import { Paper, Typography } from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
+import { Paper, Typography } from "@material-ui/core";
 
 import style from "./DropMenu.module.css";
 
 export const DropMenu = function (props) {
   return (
     <Paper
-      elevation={3}
+      elevation={6}
       className={
         props.isAccountClicked
           ? style["dropMenu--visible"]
           : style["dropMenu--hide"]
       }
     >
-      <div> Twój profil / Wyświetl profil </div>
-      <div> Ustawienia konta </div>
-      <div> Logout button </div>
+      <Link to="/profile" style={{ textDecoration: "none" }}>
+        <div className={style.dropMenu__userInfo}>
+          <div className={style["dropMenu__userInfo--avatar"]}></div>
+          <Typography className={style["dropMenu__userInfo--userName"]}>
+            Amelia Ameliowska
+          </Typography>
+        </div>
+      </Link>
+      <hr></hr>
+      <div>
+        <Typography>Ustawienia konta</Typography>
+      </div>
+      <div>
+        <Typography>Logout button</Typography>
+      </div>
     </Paper>
   );
 };
