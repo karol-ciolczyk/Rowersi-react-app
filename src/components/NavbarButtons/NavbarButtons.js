d0cb56aa830ae99249bde9942f355137bad72eb3
Karol Ciołczyk <dyweko@gmail.com>
2021-08-22 21:07:13 +0200

    home button activated, icons added to buttons

--- src/components/NavbarButtons/NavbarButtons.js
+++ src/components/NavbarButtons/NavbarButtons.js
@@ -1,7 +1,10 @@
 import React, { useContext } from "react";
 import UserSessionContext from "../context/userSession-context";
+import { useHistory } from "react-router-dom";
 
 import { Button } from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+import DirectionsBikeIcon from "@material-ui/icons/DirectionsBike";
 import SignUpModal from "../SignUpModal/SignUpModal";
 import LoginModal from "../LoginModal/LoginModal";
 import { AccountButton } from "./AccountButton";
@@ -10,7 +13,7 @@ import style from "./NavbarButtons.module.css";
 
 import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
-  button: {
+  button1: {
     margin: "0px 5px",
     backgroundColor: "Tomato",
     color: "#fff",
@@ -18,19 +21,42 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: "#db543c",
     },
   },
+  button2: {
+    margin: "0px 5px",
+    backgroundColor: "#3BB2D0",
+    color: "#fff",
+    "&:hover, &:focus": {
+      backgroundColor: "#329fba",
+    },
+  },
 }));
 
 export const NavbarButtons = function (props) {
   const classes = useStyles();
+  const history = useHistory();
   const ctx = useContext(UserSessionContext);
+
+  const onClickHandler = function () {
+    history.push("/");
+  };
+
   const navButtons = ctx.userUid ? (
     <div className={style.buttons}>
       <Button
-        className={classes.button}
+        className={classes.button1}
+        startIcon={<DirectionsBikeIcon />}
         variant="contained"
         onClick={props.newRouteButtonClickHandler}
       >
         New Route
+      </Button>
+      <Button
+        className={classes.button2}
+        variant="contained"
+        startIcon={<HomeIcon />}
+        onClick={onClickHandler}
+      >
+        Home
       </Button>
       <AccountButton />
     </div>
